e004e7cdffc21176d4d594a59d24335e03afdba1
q529665795k <q529665795@gmail.com>
2026-04-19 21:44:36 +0800

    Remove Socket.IO server initialization
    Removed Socket.IO server configuration.

--- server.js
+++ server.js
@@ -158,19 +158,6 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 6500;
 
-const io = new Server(server, {
-    cors: {
-        origin: ["https://www.im6.qzz.io", "https://im6.qzz.io"],
-        methods: ["GET", "POST"],
-        credentials: true
-    },
-    transports: ['websocket', 'polling'],
-    pingTimeout: 60000,
-    pingInterval: 25000,
-    maxHttpBufferSize: 10 * 1024 * 1024,
-    allowEI03: true
-});
-
 const dbConfig = {
   host: 'hg.sj8.xyz',
   port: 3306,
