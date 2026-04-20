1bb8b80bcf59a66573d94151cc5f84b37198ddfa
q529665795k <q529665795@gmail.com>
2026-04-20 21:40:56 +0800

    Refactor CORS middleware to simplify origin check

--- server.js
+++ server.js
@@ -379,11 +379,12 @@ rl.on('line', async (input) => {
 // ====================== 还原成你原来能用的跨域 ======================
 // 只允许你的前端域名跨域，不搞花里胡哨的
 app.use((req, res, next) => {
-  // 只允许前端地址
+
   const allowedOrigin = "https://www.im6.qzz.io";
-  if (req.headers.origin === allowedOrigin) {
-    res.header("Access-Control-Allow-Origin", allowedOrigin);
-  }
+
+    res.header("Access-Control-Allow-Origin", "https://www.im6.qzz.io");
+
+  
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   res.header("Access-Control-Allow-Credentials", "true");
