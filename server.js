813f8db7721d1932dbc182d0d2dfd2f45ef094d7
q529665795k <q529665795@gmail.com>
2026-04-28 11:55:24 +0800

    Update server.js

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.get('/api/get_user_info', async (req, res) => {
  async function callAI(prompt) {
   try {
     const res = await axios.post(
-   axios.post("https://useavnmd-mm.hf.space/api/chat"; {   
+   axios.post("https://useavnmd-mm.hf.space/api/chat"; 
 async function callAI(prompt) {
   try {
     const res = await axios.post(TARGET_URL,
